d0149f24f2728427de27937f9fc4df2d3406117d
Abel <abelmathew1504@gmail.com>
2025-02-01 21:59:21 +0530

    Initial layout, inline style error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Game from './components/Game'
 import './App.css'
 
 function App() {
@@ -8,26 +9,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <header>
+          <a href="/"><img className="logo" src="/home/abelist-cropped.png" alt="" srcSet="" /></a>
+          <h1>/</h1>
+          <h1>Games</h1>
+          <h1>/</h1>
+          <h1></h1>
+          <div className="score"><span>High score: 05</span><span>Current score: 00</span></div>
+        </header>
+        <main>
+          <Game />
+        </main>
+        <footer>Credit to The Odin Project for the project idea</footer>
+      </body>
+      </html>
     </>
   )
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,7 @@
+function Game() {
+    return (
+        <div style="width: 300px; height: 300px; background-color: black;"></div>
+    )
+}
+
+export default Game
